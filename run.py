59c2bc4d9ab7fd34793365445a367371c97333a7
YuriAlvvvss <00001090181620sp@al.educacao.sp.gov.br>
2026-08-29 23:13:17 -0300

    correções

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ app = create_app()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "0").lower() in {"1", "true", "yes", "on"}
-    app.run(host="0.0.0.0", port=port, debug=debug, use_reloader=False)
+    app.run(host="0.0.0.0", port=port, debug=debug, use_reloader=debug)
